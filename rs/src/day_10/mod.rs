7205269bfd8a0ce7b10c04a4decf035fdce721f1
Rudyard Richter <richter3141@gmail.com>
2022-12-11 11:26:50 -0700

    Add rs day 10

---
+++ rs/src/day_10/mod.rs
@@ -0,0 +1,62 @@
+#[aoc_generator(day10)]
+pub fn get_input(input: &str) -> Vec<isize> {
+    input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|s| s.parse::<isize>().unwrap_or(0))
+        })
+        .flatten()
+        .collect()
+}
+
+#[aoc(day10, part1)]
+pub fn part_1(ns: &Vec<isize>) -> isize {
+    (1..=220)
+        .zip(ns.iter())
+        .fold((0, 1), |(sum, x), (i, n)| {
+            if (i - 20) % 40 == 0 {
+                (sum + i * x, x + n)
+            } else {
+                (sum, x + n)
+            }
+        })
+        .0
+}
+
+#[aoc(day10, part2)]
+pub fn part_2(ns: &Vec<isize>) -> &'static str {
+    let letters: String = (0..)
+        .zip(ns.iter())
+        .fold((Vec::new(), 1), |(mut pixels, x), (i, n)| {
+            pixels.push(i % 40 - 1 <= x && x <= i % 40 + 1);
+            (pixels, x + n)
+        })
+        .0
+        .iter()
+        .map(|&pixel| if pixel { '█' } else { ' ' })
+        .collect::<Vec<char>>()
+        .chunks(40)
+        .intersperse(&['\n'])
+        .flatten()
+        .collect();
+    println!("{}", letters);
+    ""
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("../../test_data/day_10.txt");
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&get_input(INPUT)), 13140);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // TODO
+    }
+}
--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(iter_intersperse)]
+
 #[macro_use]
 extern crate aoc_runner_derive;
 
@@ -10,5 +12,6 @@ mod day_06;
 mod day_07;
 mod day_08;
 mod day_09;
+mod day_10;
 
 aoc_lib! { year = 2022 }
